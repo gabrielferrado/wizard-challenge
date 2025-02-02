44266083b2097f9ee0fbe5748dd51460c6aef1ef
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-02 15:20:25 -0300

    chore: update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes:  ["error", "double", { avoidEscape: true }],
+      indent: ["error", 2],
+      "quote-props": ["error", "as-needed"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "no-trailing-spaces": ["error", { ignoreComments: true }],
+      "object-curly-spacing": ["error", "always"]
+    }
+  })
 ];
 
 export default eslintConfig;
