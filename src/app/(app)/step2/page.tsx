487f66e3bd35923fb398f96d5d4d7068f382037c
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-02 17:54:24 -0300

    feat: add mandatory pages

---
+++ src/app/(app)/step2/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <p>
+        step2
+      </p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(app)/step3/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>
+        step3
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
