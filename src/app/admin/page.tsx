88a49b0e0539eb7002d44ac0e3ba7c70df90d29b
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-03 13:38:27 -0300

    refactor: manage onboarding pages inputs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,77 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return (
-    <div>
+import React, { useCallback, useEffect, useMemo } from "react";
+import OnboardingManager from "@/components/onboarding-manager";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Page } from "@/interfaces/page";
+import { Component } from "@/interfaces/component";
+import { getComponents } from "@/http/get-components";
+import { getPages } from "@/http/get-pages";
+import { updateComponents } from "@/mutations/update-components";
+
+const AdminPage = () => {
+  const {
+    isPending: isPendingComponents,
+    error: errorComponents,
+    data: components = [],
+  } = useQuery<Component[]>({
+    queryKey: ["components"],
+    queryFn: getComponents,
+  });
+
+  const {
+    isPending: isPendingPages,
+    error: errorPages,
+    data: pages = [],
+  } = useQuery<Page[]>({
+    queryKey: ["pages"],
+    queryFn: getPages,
+  });
+
+  const { mutate, isError, status } = useMutation({
+    mutationFn: updateComponents,
+    mutationKey: ["update-components"],
+  });
+
+  const isLoading = useMemo(() => (isPendingComponents || isPendingPages), [isPendingComponents, isPendingPages]);
+
+  const handleSubmit = useCallback((components: Component[]) => {
+    mutate(components);
+  }, [mutate]);
+
+  useEffect(() => {
+    if (status === "success") alert("Components updated successfully!");
+  }, [status]);
+
+  if (isLoading) {
+    return (
       <h1>
-        Admin page
+        Loading...
       </h1>
-    </div>
+    );
+  }
+
+  if (errorPages || errorComponents || isError) {
+    console.log(errorPages);
+    return (
+      <pre>
+        Unexpected Error
+      </pre>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white text-center">
+          Admin page
+        </h1>
+      </header>
+      <main>
+        <OnboardingManager { ...{ pages, components, handleSubmit } } />
+      </main>
+    </>
   );
 };
 
-export default Page;
+export default AdminPage;
