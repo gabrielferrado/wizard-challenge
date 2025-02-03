a954f2dd60186753f964a81658486a87da6b96fc
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-03 12:52:49 -0300

    test: add admin page

--- __tests__/pages/Admin.test.tsx
+++ __tests__/pages/Admin.test.tsx
@@ -1,9 +1,65 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import * as ReactQuery from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
 import AdminPage from "@/app/admin/page";
 
-describe("Admin Page", () => {
-  it("should render correctly", () => {
+global.alert = jest.fn();
+
+jest.mock("@tanstack/react-query", () => {
+  const actual = jest.requireActual<typeof ReactQuery>("@tanstack/react-query");
+
+  return {
+    ...actual,
+    useQuery: jest.fn(),
+    useMutation: jest.fn(),
+  };
+});
+
+describe("AdminPage", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (useQuery as jest.Mock).mockReturnValue({
+      data: [{ id: 1, name: "some-component", pageId: 1 }],
+      isPending: false,
+      error: null,
+    });
+    (useMutation as jest.Mock).mockReturnValue({
+      mutate: jest.fn(),
+      isError: false,
+      status: "idle",
+    });
+  });
+
+  it("renders page successfully", () => {
     render(<AdminPage />);
+
     expect(screen).toMatchSnapshot();
+  });
+
+  it("renders loading state when queries are pending", () => {
+    (useQuery as jest.Mock).mockImplementation(() => ({
+      isPending: true,
+      error: null,
+      data: [],
+    }));
+
+    render(<AdminPage />);
+
+    expect(screen.getByText("Loading...")).toBeTruthy();
+  });
+
+  it("renders error state if queries fail", () => {
+    (useQuery as jest.Mock).mockImplementation(() => ({
+      isPending: false,
+      error: new Error("Failed to load"),
+      data: [],
+    }));
+
+    render(<AdminPage />);
+
+    expect(screen.getByText("Unexpected Error")).toBeTruthy();
   });
 });
