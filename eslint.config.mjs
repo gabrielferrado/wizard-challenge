b0aae804eb8f395fa5cfb91420c70703e27ab4db
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-04 19:24:25 -0300

    chore: update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,6 @@ const eslintConfig = [
       "react/jsx-indent-props": ["error", 2],
       "react/jsx-one-expression-per-line": ["error"],
       "react/jsx-first-prop-new-line": ["error", "multiline"],
-      "react/jsx-max-props-per-line": ["error", { maximum: 1, when: "always" }],
       "react/jsx-closing-bracket-location": [
         "error",
         { selfClosing: "line-aligned", nonEmpty: "line-aligned" }
