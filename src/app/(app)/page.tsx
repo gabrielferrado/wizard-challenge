4f6cfdaa09772363f758a97e0b8705dde44b74c0
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-04 19:12:33 -0300

    refactor: update onboarding home page to handle sessions

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,62 +1,31 @@
-import Stepper from "@/components/stepper";
+import InitialForm from "@/components/initial-form";
+import { cookies } from "next/headers";
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("SESSION");
+
+  if (session && session.value) {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: session.value,
+      },
+    });
+
+    if (user?.currentStep === 1) return redirect("/step2");
+    if (user?.currentStep === 2) return redirect("/step3");
+  }
+
   return (
     <>
-      <header className="container w-full sm:w-[60%]">
-        <Stepper/>
-      </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full sm:w-[30%]">
-        <h1
-          className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white text-center mb-16"
-        >
-          Welcome to Zealthy Onboarding!
-        </h1>
-        <form className="w-full">
-          <div className="mb-5">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
-            </label>
-            <input
-              data-testid='email'
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@flowbite.com"
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your password
-            </label>
-            <input
-              data-testid='password'
-              type="password"
-              id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="********"
-              required
-            />
-          </div>
-
-        </form>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <button
-          data-testid='submitButton'
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </footer>
+      <h1
+        className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white text-center mb-16"
+      >
+        Welcome to Zealthy Onboarding!
+      </h1>
+      <InitialForm/>
     </>
   );
 }
