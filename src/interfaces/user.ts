9e1c59e281dd2685ec67d0c907cffe243873d86c
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-04 16:41:28 -0300

    feat: add user interface

---
+++ src/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  email: string
+  password: string
+  aboutMe: string
+  streetAddress: string
+  city: string
+  state: string
+  zip: string
+  birthdate: string
+  currentStep: number
+}
