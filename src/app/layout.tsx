5f96399fe60a3433695612f0b04fce9741c2dcab
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-04 19:09:19 -0300

    refactor: use overlay to block interaction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/app/providers";
+import Overlay from "@/components/overlay";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
         >
           <Providers>
+            <Overlay />
             {children}
           </Providers>
         </div>
