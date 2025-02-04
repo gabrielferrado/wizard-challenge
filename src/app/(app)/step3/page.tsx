91e80d41c11a6063ec686fa6cff6e0d4525ce656
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-04 19:13:38 -0300

    refactor: update third step to fetch inputs

--- src/app/(app)/step3/page.tsx
+++ src/app/(app)/step3/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { prisma } from "@/lib/db";
+import ControlledForm from "@/components/controlled-form";
 
-const Page = () => {
-  return (
-    <div>
-      <h1>
-        step3
-      </h1>
-    </div>
-  );
+export default async function Step3Page() {
+  const pageController = await prisma.onboardingPage.findFirst({ where: { pageNumber: 2 }, include: { components: true } });
+
+  if (!pageController) {
+    return (
+      <pre>
+        Could not find the page controller!
+      </pre>
+    );
+  }
+
+  return <ControlledForm controller={pageController} />;
 };
-
-export default Page;
