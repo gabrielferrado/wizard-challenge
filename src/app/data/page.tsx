f2de25f280f7e68f07d7d2a72bcfcc0ac6b33a0e
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-05 12:36:24 -0300

    refactor: force dynamic to handle prod env

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { prisma } from "@/lib/db";
 import sanitizeAddress from "@/util/sanitize-address";
 
+export const dynamic = "force-dynamic";
+
 export default async function DataPage (){
   const users = await prisma.user.findMany();
 
