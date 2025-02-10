9388dddb41f325420a3714e37721720bd14eab8e
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-10 20:58:38 -0300

    test: make controlled form generic

--- __tests__/components/controlled-form.test.tsx
+++ __tests__/components/controlled-form.test.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import { useOnboarding } from "@/hooks/use-onboarding";
 import ControlledForm from "@/components/controlled-form";
 
 jest.mock("../../src/hooks/use-onboarding", () => ({
   useOnboarding: jest.fn(),
+  OnboardingAvailableForms: { SOME_COMPONENT: jest.fn() }
 }));
 
 const mockRegister = jest.fn();
@@ -15,7 +16,6 @@ const mockValidatePartial = jest.fn();
 describe("ControlledForm", () => {
   beforeEach(() => {
     jest.clearAllMocks();
-
     (useOnboarding as jest.Mock).mockReturnValue({
       register: mockRegister,
       handleSubmit: mockHandleSubmit,
@@ -23,10 +23,10 @@ describe("ControlledForm", () => {
     });
   });
 
-  it("renders fallback if controller.components is empty or undefined", () => {
+  it("should render fallback if controller.components is empty or undefined", () => {
     const mockController = {
       id: 1,
-      components: [],
+      components: undefined,
     } as never;
 
     render(<ControlledForm controller={mockController} />);
@@ -35,80 +35,25 @@ describe("ControlledForm", () => {
     expect(submitButton).toBeNull();
   });
 
-  it("renders ABOUT_ME fields if included", () => {
+  it("should render correctly", () => {
     const mockController = {
       id: 1,
-      components: [
-        { componentName: "ABOUT_ME" },
-      ],
+      components: [{ componentName: "SOME_COMPONENT" }],
     } as never;
 
     render(<ControlledForm controller={mockController} />);
-    expect(screen.getByText("About you")).toBeInTheDocument();
-    expect(screen.getByTestId("aboutMe")).toBeInTheDocument();
-    expect(screen.queryByLabelText("Street address")).toBeNull();
-    expect(screen.queryByLabelText("Birthdate")).toBeNull();
+    expect(screen).toMatchSnapshot();
   });
 
-  it("renders ADDRESS fields if included", () => {
+  it("should warn when component is not available", () => {
     const mockController = {
       id: 1,
-      components: [
-        { componentName: "ADDRESS" },
-      ],
+      components: [{ componentName: "TEST" }],
     } as never;
 
-    render(<ControlledForm controller={mockController} />);
-
-    expect(screen.getByLabelText("Street address")).toBeInTheDocument();
-    expect(screen.getByLabelText("City")).toBeInTheDocument();
-    expect(screen.getByLabelText("State")).toBeInTheDocument();
-    expect(screen.getByLabelText("ZIP code")).toBeInTheDocument();
-    expect(screen.queryByTestId("aboutMe")).toBeNull();
-    expect(screen.queryByLabelText("Birthdate")).toBeNull();
-  });
-
-  it("renders BIRTHDATE field if included", () => {
-    const mockController = {
-      id: 1,
-      components: [
-        { componentName: "BIRTHDATE" },
-      ],
-    } as never;
+    const spyWarn = jest.spyOn(console, "warn");
 
     render(<ControlledForm controller={mockController} />);
-    expect(screen.getByLabelText("Birthdate")).toBeInTheDocument();
-    expect(screen.queryByTestId("aboutMe")).toBeNull();
-    expect(screen.queryByLabelText("Street address")).toBeNull();
-  });
-
-  it("renders multiple inputs if multiple componentNames are included", () => {
-    const mockController = {
-      id: 1,
-      components: [
-        { componentName: "ABOUT_ME" },
-        { componentName: "ADDRESS" },
-        { componentName: "BIRTHDATE" },
-      ],
-    } as never;
-
-    render(<ControlledForm controller={mockController} />);
-    expect(screen.getByTestId("aboutMe")).toBeInTheDocument();
-    expect(screen.getByLabelText("Street address")).toBeInTheDocument();
-    expect(screen.getByLabelText("Birthdate")).toBeInTheDocument();
-  });
-
-  it("submits the form and calls handleSubmit", () => {
-    const mockController = {
-      id: 1,
-      components: [
-        { componentName: "ABOUT_ME" },
-      ],
-    } as never;
-
-    render(<ControlledForm controller={mockController} />);
-
-    fireEvent.click(screen.getByTestId("submitButton"));
-    expect(mockHandleSubmit).toHaveBeenCalledTimes(1);
+    expect(spyWarn).toHaveBeenCalledWith("Component \"TEST\" is not available.");
   });
 });
