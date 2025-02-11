9aafc24850a93362984a298b4e3363d3fd499344
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-02-10 21:00:11 -0300

    style: enhance visual feedback

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import { useOnboarding } from "@/hooks/use-onboarding";
+import SPINNER from "../../public/spinner.svg";
+import Image from "next/image";
 
 const Overlay = () => {
   const { isLoading } = useOnboarding();
   return isLoading ? (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
-      <p className="text-gray-200 text-lg font-semibold">
-        Please wait...
-      </p>
+      <Image src={SPINNER} alt='Loading' />
     </div>
   ) : null;
 };
